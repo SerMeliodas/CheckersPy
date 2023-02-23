b51182af81cf478ea5cdc3c3ca3604b343b4d152
unknown <oliver.shved@elbaci.sk>
2023-02-23 14:46:40 +0100

    added board initiantion and requirements.txt

--- board.py
+++ board.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import numpy as np
+
 from pprint import pprint
 from checker import Checker
 
@@ -8,19 +10,33 @@ class Board(pygame.sprite.Sprite):
     def __init__(self, board_size: int):
         super().__init__()
 
+        self.checker_group = pygame.sprite.Group()
+
         self.image = pygame.image.load(os.getenv("BOARD_IMAGE", ""))
-        self.image = pygame.transform.scale_by(self.image, board_size / self.image.get_width())
+        self.image = pygame.transform.scale_by(
+            self.image, board_size / self.image.get_width())
 
         self.rect = self.image.get_rect(topleft = (0, 0))
         self.board = self.init()
         pprint(self.board)
 
     def init(self):
-        board = [[0]*8]*8
+
+        def create_checkers(row: int, col: int, board: list):
+            if row % 2 == 0 and col % 2 == 0:
+                board[row][col] = Checker((row, col), self.checker_group)
+            if row % 2 != 0 and col % 2 != 0:
+                board[row][col] = Checker((row, col), self.checker_group)
+
+        board = np.zeros((8, 8), dtype=object)
 
         for row in range(8):
             for col in range(8):
-                pass
+                if row < 3:
+                    create_checkers(row, col, board)
+                
+                if row > 4:
+                    create_checkers(row, col, board)
 
         return board
 
--- checker.py
+++ checker.py
@@ -2,5 +2,6 @@ import pygame
 
 
 class Checker(pygame.sprite.Sprite):
-    def __init__(self, pos: tuple):
-        super().__init__()
+    def __init__(self, pos: tuple, group):
+        super().__init__(groups=group)
+        self.pos = pos
